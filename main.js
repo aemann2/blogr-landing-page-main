1bb29ff45772b18817c4f71df5efeb392b067834
Adam Mann <aemann2@gmail.com>
2021-07-21 12:00:28 -0700

    final styling of mobile header dropdown

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 
 hamburger.addEventListener('click', () => {
-	let bgImage = hamburger.style.backgroundImage
+	let bgImage = hamburger.style.backgroundImage;
 	if (!bgImage || bgImage === 'url("../images/icon-hamburger.svg")') {
 		hamburger.style.backgroundImage = 'url("../images/icon-close.svg")';
 	} else {
